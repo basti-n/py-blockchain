de291c19a4b628548a9f8cfa0147abfa764ae51d
Sebastian <sebastian.neumair@gmail.com>
2021-05-03 06:33:08 +0200

    Add better printing of block ('Print Blocks')

--- utils/mixins/orderDictMixin.py
+++ utils/mixins/orderDictMixin.py
@@ -4,3 +4,6 @@ from collections import OrderedDict
 class OrderedDictMixin:
     def __str__(self) -> str:
         return str(OrderedDict(self.__dict__.items()))
+
+    def __repr__(self) -> str:
+        return str([f'{key}: {value}' for key, value in self.__dict__.items()]).replace('\'', '')

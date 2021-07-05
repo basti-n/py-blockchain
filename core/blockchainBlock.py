a5a1b4c53d84f651c9883653240a56b286725fb1
Sebastian <sebastian.neumair@gmail.com>
2021-07-05 19:20:33 +0200

    Lint blockchainBlock.py (PEP8)

--- core/blockchainBlock.py
+++ core/blockchainBlock.py
@@ -8,18 +8,20 @@ def is_next_block(blockchain: Blockchain, block: Block) -> bool:
 
 
 def is_future_block(blockchain: Blockchain, block: Block) -> bool:
-    """ Checks whether the block is larger than the next expected block in the chain """
+    """ Checks whether the block is larger than
+    the next expected block in the chain """
     return block.index > get_latest_block_index(blockchain) + 1
 
 
 def is_previous_block(blockchain: Blockchain, block: Block) -> bool:
-    """ Checks whether the block is larger than the next expected block in the chain """
+    """ Checks whether the block is larger than
+    the next expected block in the chain """
     return block.index < get_latest_block_index(blockchain) + 1
 
 
 def get_latest_block_index(blockchain: Blockchain) -> int:
     """ Returns the index of the latest block in blockchain """
-    if blockchain.latest_block != None:
+    if blockchain.latest_block is not None:
         return blockchain.latest_block.index
 
     return 0
